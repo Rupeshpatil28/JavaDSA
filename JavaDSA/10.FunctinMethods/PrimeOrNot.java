637d4efcd298e9a67c2b39d3ef6b10825374a4de
Rupesh Patil <bagalrupesh28@gmail.com>
2026-05-22 11:52:29 +0530

    update

--- JavaDSA/10.FunctinMethods/PrimeOrNot.java
+++ JavaDSA/10.FunctinMethods/PrimeOrNot.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class PrimeOrNot {
 
 //    public static boolean isPrime(int n) {
@@ -7,7 +6,7 @@ public class PrimeOrNot {
 //                return false;
 //            }
 //        }
-//        return isPrime;
+//        return true;
 //    }
 
     //In Optimised way
@@ -32,38 +31,3 @@ public class PrimeOrNot {
          PrimePrint(20);
     }
 }
-=======
-public class PrimeOrNot {
-
-//    public static boolean isPrime(int n) {
-//        for(int i = 2; i<=n-1; i++) {
-//            if (n % i == 0) {
-//                return false;
-//            }
-//        }
-//        return isPrime;
-//    }
-
-    //In Optimised way
-    public static boolean isPrime(int n) {;
-        for (int i = 2; i<=Math.sqrt(n); i++) {   //optimised way Using math
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //Print all primes in a range
-    public static void PrimePrint(int n) {
-        for (int i=2; i<=n; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
-            }
-        }
-    }
-    public static void main(String args[]) {
-         PrimePrint(20);
-    }
-}
->>>>>>> 2602d06755d11123f2a36cf586bb7d6e8a104877
---
+++ JavaDSA/TCS.CODING.QUE/Prime.java
@@ -0,0 +1,14 @@
+public class Prime {
+    public static boolean checkprime (int n) {
+        for(int i=2; i<n-1; i++) {
+            if (n % i == 0) {
+                return false;
+            } 
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        int n = 5;
+        System.out.println(checkprime(n));
+    }
+}
